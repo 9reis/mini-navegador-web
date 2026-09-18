1567a4cb0f67e9329458ff2715979e5a3d7fa422
9reis <lucasreis_9@hotmail.com>
2026-09-18 17:09:30 -0300

    Cria a app

---
+++ MiniNavegador.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage; 
+
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class MiniNavegador extends Application { 
+
+	@Override
+	public void start(Stage palco){
+		TextField campoUrl = new TextField();
+		WebView navegador = new WebView();
+		WebEngine motor = navegador.getEngine();
+
+		// Carrega uma pag. da web ao pressionar enter 
+		campoUrl.setOnAction(evento -> motor.load( campoUrl.getText()));
+
+		VBox vbox = new VBox(); 
+		vbox.getChildren().addAll(campoUrl, navegador);
+		Scene cena = new Scene(vbox);
+
+		palco.setTitle("Meu Browser Java");
+		palco.setScene(cena);
+		palco.show();
+	}
+
+	public static void main(String[] args){
+		launch(args);
+	}
+}
